0082c87586611aaf794b095772ba30b4d28eb280
cryptojackpotby2jzcoin <cafflions@gmail.com>
2025-02-02 00:01:04 -0500

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -7,7 +7,7 @@ if (typeof window.Buffer === "undefined") {
 const connection = new solanaWeb3.Connection("https://api.testnet.solana.com", "confirmed");
 
 // 2JZ Coin mint adresi
-const tokenMintAddress = new solanaWeb3.PublicKey("GRjLQ8KXegtxjo5P2C2Gq71kEdEk3M");
+const tokenMintAddress = new solanaWeb3.PublicKey("GRjLQ8KXegtxjo5P2C2Gq71kEdEk3mLVCMx4AARUpump");
 
 // House Wallet Public Key
 const houseWalletPublicKey = new solanaWeb3.PublicKey("6iRYHMLHpUBrcnfdDpLGvCwRutgz4ZAjJMSvPJsYZDmF");
