5cb90e3c79e4dfd7b8159ebea5484282e7c958d4
cryptojackpotby2jzcoin <cafflions@gmail.com>
2025-03-19 20:38:24 -0400

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const spinButton = document.getElementById("spin-button");
   const transferButton = document.getElementById("transfer-button");
 
-  const programId = new window.solanaWeb3.PublicKey("");
+  const programId = new window.solanaWeb3.PublicKey("8GeDy4btKEUvqyLAoqUzTHBfdAV3sn1dE7MDYPvjuhVn");
   const tokenMint = new window.solanaWeb3.PublicKey("GRjLQ8KXegtxjo5P2C2Gq71kEdEk3mLVCMx4AARUpump");
   let userWallet = null;
 
